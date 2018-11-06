d936523c34b5adbbd631ce1a919dd08867ca5635
zhmiblog <zhmiblog@gmail.com>
2018-11-06 15:31:39 +0800

    ss

--- src/components/face.vue
+++ src/components/face.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="face" class="center">
-	<p><img :src="url" /></p>
+    <p><img :src="url" height="200" /></p>
     <input type="file" id="file" ref="file" class="inputfile" @change="changepic" accept="image/png, image/jpeg, image/gif, image/jpg" /> 
     <label for="file" class="weui-btn weui-btn_primary">Choose a file</label> 
   </div>
@@ -19,6 +19,7 @@ export default {
         //console.log($ref.file.files[0]);//这里可以获取上传文件的name
         var newsrc=this.$options.methods.getObjectURL(this.$refs.file.files[0]);
         this.url=newsrc;
+        this.$options.methods.submit(this.$refs.file.files[0]);
     },
     //建立一個可存取到該file的url
     getObjectURL(file) {
@@ -32,10 +33,27 @@ export default {
             url = window.webkitURL.createObjectURL(file) ;
         }
         return url ;
+    },
+    
+    submit(file){
+       event.preventDefault();//取消默认行为
+       let formdata = new FormData();
+       formdata.append('file',file);
+       let config = {
+          headers: {
+          'Content-Type': 'multipart/form-data' //之前说的以表单传数据的格式来传递fromdata
+           }
+        };
+       console.log(this.$vm)
+       console.log(this)
+       this.axios.post('/upload', formdata, config).then( (res) => {
+          console.log(res)
+       }).catch((error) =>{
+       console.log(error)
+        });
     }
-
-    }
-	
+  }
+ 
 }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import router from './router'
-import './assets/weui.css'
+import './assets/weui.min.css'
 import App from './App.vue'
+import axios from 'axios'
 
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
 
 new Vue({
